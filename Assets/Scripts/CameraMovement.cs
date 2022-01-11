0c60f20323457b45bde85e61bbbe77514119954b
Jacob De Hoyos <dehoyos.jacob@gmail.com>
2022-01-10 22:55:09 -0600

    Create Player Controller
    Write script that allows the player to move using WASD and jump with the spacebar. Also implemented a script that allows the player to move the camera using their mouse to create a first person play experience.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] float mouseSensitivity = 100f;
+    [SerializeField] Transform playerBody;
+
+    float mouseX = 0;
+    float mouseY = 0;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 50f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance;
+    [SerializeField] float speed;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float gravity;
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] CharacterController controller;
+
+    bool isGrounded;
+    Vector3 velocity;
+    Vector3 move;
+    float x;
+    float y;
+    float z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Debug.Log("JUMP!");
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 24f;
+        }
+
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 12f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
